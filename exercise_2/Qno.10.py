337b601aaeba4af32625e8c00e83da367826213f
Kishan Prasad Jaiswal <bhootk123@gmail.com>
2021-05-03 20:20:08 +0545

    123

---
+++ exercise_2/Qno.10.py
@@ -0,0 +1,12 @@
+# Write a python program to sum of three given integer. However,if two values are equal sum will be zero.
+
+def sum(a,b,c):
+    if a==b or b==c or c==a:
+        sum=0;
+    else:
+        sum=(a+b+c)
+    return sum
+x,y,z=[int(a) for a in input ("enter three number: ")]
+print(sum(x,y,z))
+
+n=int(input("enter three number: "))
