778ed3bdef6124c3b54813812ca25ed174dbd964
Kishan Prasad Jaiswal <bhootk123@gmail.com>
2021-05-05 06:51:12 +0545

    123

---
+++ exercise_2/Qno.15.py
@@ -0,0 +1,15 @@
+# What will be the following if a= 2, b=3,c=4,d=3?
+
+a=2
+b=3
+c=4
+d=3
+b/=d
+print(a==b)
+print(a!=d)
+print(a!=c)
+print(b)
+print(b>a)
+print(a<d)
+print(b-a==c+b)
+print(b>=d)
